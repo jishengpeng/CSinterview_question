b855bbed383ac0b5087becd061cd5ae56558a9c1
jishengpeng <1749104005@qq.com>
2022-08-23 20:45:31 +0800

    字符串匹配

---
+++ Algorithm/暴力字符串匹配.cpp
@@ -0,0 +1,39 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <cstring>
+using namespace std;
+
+//写一个暴力算法的字符串匹配
+
+int main() {
+	string str1, str2;
+	getline(cin, str1);
+	getline(cin, str2);
+	int len1 = str1.length();
+	int len2 = str2.length();
+	//判断主串的位置和字串的位置
+	int i = 0, j = 0,k=0;
+	while (i <= len1 - len2) {
+		if (j == len2) {
+			cout << "能够找到";
+			return 0;
+		}
+		else {
+			if (str1[k] == str2[j]) {
+				k++;
+				j++;
+			}
+			else {
+				j = 0;
+				i = i + 1;
+				k = i;
+			}
+		}
+	}
+	cout << "不能找到";
+
+}
